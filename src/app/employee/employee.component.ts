7ecda4e561092b87b0b33385623d8161056085ea
7estelle <layestelle7@gmail.com>
2022-06-14 23:57:13 +0200

    create Employee component

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import employeesData from '../../assets/data/employees.json';
+
+interface Employee {  
+  FirstName: String;  
+  SecondName: String;  
+  Position: String;  
+  Id: String; 
+  Details: String;  
+  Blocked: boolean;   
+}  
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.scss']
 })
-export class EmployeeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class EmployeeComponent {
+  employees = employeesData;
 }
--- src/app/employees-list/employees-list.component.ts
+++ src/app/employees-list/employees-list.component.ts
@@ -1,15 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import employees from '../../assets/data/employees.json'; 
-import { EmployeeComponent } from '../employee/employee.component'; 
-
-interface Employee {  
-  FirstName: String;  
-  SecondName: String;  
-  Position: String;  
-  Id: String; 
-  Details: String;  
-  Blocked: boolean;   
-}  
 
 @Component({
   selector: 'app-employees-list',
@@ -17,9 +6,5 @@ interface Employee {
   styleUrls: ['./employees-list.component.scss']
 })
 export class EmployeesListComponent {
-
-  name = 'Angular';  
-    
-  employees: Employee[] = employees; 
 
 }
