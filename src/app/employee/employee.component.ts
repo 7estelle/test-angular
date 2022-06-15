0f272d11ef24998f2dd2c1777eaf45acca0362a6
7estelle <layestelle7@gmail.com>
2022-06-15 22:14:07 +0200

    STATE 1 - Main Objectives -Done

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import employeesData from '../../assets/data/employees.json';
-
+ 
 interface Employee {  
   FirstName: String;  
   SecondName: String;  
   Position: String;  
-  Id: String; 
+  Id: String;
   Details: String;  
-  Blocked: boolean;   
+  Blocked: boolean;  
 }  
-
+ 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.scss']
 })
+ 
 export class EmployeeComponent {
-  employees = employeesData;
+  @Input() inputFromParent : Array<Employee>;
+  @Input() index: number;
+  constructor() { }
+
 }
--- src/app/employees-list/employees-list.component.ts
+++ src/app/employees-list/employees-list.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import employeesData from '../../assets/data/employees.json';
+ 
+interface Employee {  
+  FirstName: String;  
+  SecondName: String;  
+  Position: String;  
+  Id: String;
+  Details: String;  
+  Blocked: boolean;  
+}  
+ 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
   styleUrls: ['./employees-list.component.scss']
 })
 export class EmployeesListComponent {
-
+  employees = employeesData;
+  inputText = this.employees;
 }
